f9a93f6b93cf1eedf432cd2bc388cca8a48db554
nogarremi <nogarremi@nogarremi.com>
2020-02-20 21:51:27 -0800

    Changing Ranks to be per match
    Given the fact that opponents change rank; it is better to track per match than per opponent e.g., playerA is Bronze but dropped to Rookie and back to Bronze to S. Bronze later. He's a different player when he hits S. Bronze

--- resources/js/sfvlog.js
+++ resources/js/sfvlog.js
@@ -53,7 +53,7 @@ function sortMatches (season = 0, type = 0, my_char = 0, opp = 0, opp_rank = 0, 
     
     
     for (var i in matches){
-        if ((matches[i]['season'] == season || season == 0) && (matches[i]['match_type'] == type || type == 0) && (matches[i]['My Character']['char_id'] == my_char || my_char == 0) && (matches[i]['opponent']['opp_id'] == opp || opp == 0) && (matches[i]['opponent']['rank']['rank_id'] == opp_rank || opp_rank == 0) && (matches[i]['Opponent Character']['char_id'] == opp_char || opp_char == 0) && (matches[i]['result'] == result || result == 0)){
+        if ((matches[i]['season'] == season || season == 0) && (matches[i]['match_type'] == type || type == 0) && (matches[i]['My Character']['char_id'] == my_char || my_char == 0) && (matches[i]['opponent']['opp_id'] == opp || opp == 0) && (matches[i]['rank']['rank_id'] == opp_rank || opp_rank == 0) && (matches[i]['Opponent Character']['char_id'] == opp_char || opp_char == 0) && (matches[i]['result'] == result || result == 0)){
             var match_type = '';
             var win_loss = '';
             var rank_name = '';
@@ -76,12 +76,12 @@ function sortMatches (season = 0, type = 0, my_char = 0, opp = 0, opp_rank = 0, 
                     win_loss = "Loss";
                     break;
             }
-            switch (matches[i]['opponent']['rank']['rank_id']) {
+            switch (matches[i]['rank']['rank_id']) {
                 case 19:
                     rank_name = "Ult. Grand Master";
                     break;
                 default:
-                    rank_name = matches[i]['opponent']['rank']['rank_name'];
+                    rank_name = matches[i]['rank']['rank_name'];
             }
         
             resultHTML += "<div id='" + matches[i]['match_id'] + "' class='row'><div class='col-xs-1 ranbats'>" + matches[i]['season'] + "</div><div class='col-xs-1 ranbats'>" + match_type + "</div><div class='col-xs-2 ranbats'>" + matches[i]['My Character']['char_name'] + "</div><div class='col-xs-3 ranbats'>" + matches[i]['opponent']['opp_name'] + "</div><div class='col-xs-2 ranbats'>" + rank_name + "</div><div class='col-xs-2 ranbats'>" + matches[i]['Opponent Character']['char_name'] + "</div><div class='col-xs-1 ranbats'>" + win_loss + "</div></div>"
